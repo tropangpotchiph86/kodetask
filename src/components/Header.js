9fef12ad82610507e75b353fb80b6db2708b669b
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-24 10:59:59 +0800

    Starter Files

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import Logo from '../assets/logo.png';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img
+          src={Logo}
+          alt="KodeTask Logo"
+        />
+        <span>KodeTask</span>
+      </div>
+      <div className="themeSelector">
+        <span className="light"></span>
+        <span className="medium"></span>
+        <span className="dark activeTheme"></span>
+        <span className="gOne"></span>
+        <span className="gTwo"></span>
+        <span className="gThree"></span>
+      </div>
+    </header>
+  );
+};
