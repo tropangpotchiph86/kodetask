efa628fbf90af3ee1130906a06cbe92b7c8c0169
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-24 16:54:58 +0800

    mekus mekus

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import { Header } from "./components/Header";
-import { AddTask } from "./components/AddTask";
-import { ShowTask } from "./components/ShowTask";
+import { useEffect, useState } from 'react';
+import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
+import { ShowTask } from './components/ShowTask';
 
 import './App.css';
 
 function App() {
+  const [tasklist, setTasklist] = useState(
+    JSON.parse(localStorage.getItem('tasklist')) || []
+  );
+  const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem('tasklist', JSON.stringify(tasklist));
+  }, [tasklist]);
+
   return (
     <div className="App">
       <Header />
-      <AddTask />
-      <ShowTask />
+      <AddTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
     </div>
   );
 }
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,31 +1,46 @@
-export const ShowTask = () => {
-    const tasks = [
-        {id: 10001, name: "TASK A", time: "2:09:01 AM 9/14/2030"},
-        {id: 10002, name: "TASK B", time: "2:09:01 AM 9/14/2030"},
-        {id: 10003, name: "TASK C", time: "2:09:01 AM 9/14/2030"},
-    ]
+export const ShowTask = ({ tasklist, setTasklist, task, setTask }) => {
+  const handleEdit = (id) => {
+    const selectedTask = tasklist.find((todo) => todo.id === id);
+    setTask(selectedTask);
+  };
+
+  const handleDelete = (id) => {
+    const updatedTasklist = tasklist.filter((todo) => todo.id !== id);
+    setTasklist(updatedTasklist);
+  };
 
   return (
     <section className="showTask">
-        <div className="head">
-            <div>
-                <span className="title">Todo</span>
-                <span className="count">0</span>
-            </div>
-            <button className="clearAll">Clear All</button>
+      <div className="head">
+        <div>
+          <span className="title">Todo</span>
+          <span className="count">{tasklist.length}</span>
         </div>
-        <ul>
-            { tasks.map((task) => (
-                <li>
-                    <p>
-                        <span className="name">{task.name}</span>
-                        <span className="time">{task.time}</span>
-                    </p>
-                    <i className="bi bi-pencil-square"></i>
-                    <i className="bi bi-trash"></i>
-                </li>
-            )) }            
-        </ul>
+        <button
+          className="clearAll"
+          onClick={() => setTasklist([])}
+        >
+          Clear All
+        </button>
+      </div>
+      <ul>
+        {tasklist.map((todo) => (
+          <li key={todo.id}>
+            <p>
+              <span className="name">{todo.name}</span>
+              <span className="time">{todo.time}</span>
+            </p>
+            <i
+              onClick={() => handleEdit(todo.id)}
+              className="bi bi-pencil-square"
+            ></i>
+            <i
+              onClick={() => handleDelete(todo.id)}
+              className="bi bi-trash"
+            ></i>
+          </li>
+        ))}
+      </ul>
     </section>
-  )
-}
+  );
+};
